56b51a4742487af5b1d87593bc25415bbb0e3e0a
HUSHENGLIN <244768439@qq.com>
2019-06-22 22:25:09 +0800

    提交代码

---
+++ MyTest/src/main/java/Demo.java
@@ -0,0 +1,12 @@
+/**
+ * @className: Demo
+ * @description:
+ * @author: Mr.Hu
+ * @create: 2019-06-22 22:14
+ **/
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
